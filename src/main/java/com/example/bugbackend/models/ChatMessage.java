57899eef7dcfb78d18913cd1fb528bed40db5411
Jacob Pagan <jake2415@gmail.com>
2022-01-26 21:45:55 -0600

    This is maybe for later after the course

---
+++ src/main/java/com/example/bugbackend/models/ChatMessage.java
@@ -0,0 +1,40 @@
+package com.example.bugbackend.models;
+
+import java.awt.*;
+
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private TrayIcon.MessageType type;
+
+
+    public enum MessageType {
+        CHAT, LEAVE, JOIN
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public TrayIcon.MessageType getType() {
+        return type;
+    }
+
+    public void setType(TrayIcon.MessageType type) {
+        this.type = type;
+    }
+
+
+}
