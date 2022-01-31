ffb885d2ada58d94f1beeeb78d3b42633039dbfd
Jacob Pagan <jake2415@gmail.com>
2022-01-31 10:21:31 -0600

    Done for the most part now to deploy

--- src/main/java/com/example/bugbackend/repository/BugRepository.java
+++ src/main/java/com/example/bugbackend/repository/BugRepository.java
@@ -4,4 +4,6 @@ import com.example.bugbackend.models.Bug;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BugRepository extends JpaRepository<Bug, Long> {
+    Bug findBugByUserId(Long id);
+    Bug findBugByUserIdAndTitle(Long id, String title);
 }
--- src/main/java/com/example/bugbackend/repository/UserRepository.java
+++ src/main/java/com/example/bugbackend/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
     User findUserByEmail(String email);
     Boolean existsByEmail(String email);
-    User findUserById(Long id);
+    User findUserByIdAndEmail(Long id, String email);
 }
